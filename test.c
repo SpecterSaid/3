62b499c2f15ec45b7f804a3afed482eea7747aae
Your Name <you@example.com>
2021-06-03 20:08:48 +0100

    first commit

---
+++ test.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"ft.h"
+int ft_add(int a,int b);
+void main() {
+	   int num1 = 10, num2 = 10, num3;
+	      num3 = ft_add(num1, num2);
+	         printf("Addition of Two numbers : %d", num3);
+}
